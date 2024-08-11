9233095cb267419f05beb3e99c158f636ff8caf0
Pedro Ivo Hudson <ivo.pedro@gmail.com>
2024-08-11 10:07:19 -0300

    update index content

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var index =
 [
     [ "Official links", "index.html#autotoc_md16", null ],
-    [ "Changelog", "index.html#autotoc_md17", null ]
+    [ "Community links", "index.html#autotoc_md17", null ],
+    [ "Changelog", "index.html#autotoc_md18", null ],
+    [ "About", "index.html#autotoc_md19", null ]
 ];
